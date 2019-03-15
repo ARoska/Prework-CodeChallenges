5a7125867431c80cb22bc4f7551205a8895da7d3
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-15 16:30:35 -0700

    Starting on challenge 4

---
+++ SumOfRows/SumOfRows/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumOfRows
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m;
+            int n;
+
+            Console.Write("Input the number of rows in the matrix: ");
+            Int32.TryParse(Console.ReadLine(), out m);
+
+            Console.Write("Input the number of columns in the matrix: ");
+            Int32.TryParse(Console.ReadLine(), out n);
+
+
+        }
+    }
+}
