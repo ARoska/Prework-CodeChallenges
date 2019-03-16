d848439de3c28584c91518ace959ed543bcb6a9f
Andrew Roska <andrew.l.roska@gmail.com>
2019-03-15 17:29:57 -0700

    Solves challenge 4.

--- SumOfRows/SumOfRows/Program.cs
+++ SumOfRows/SumOfRows/Program.cs
@@ -15,7 +15,33 @@ namespace SumOfRows
             Console.Write("Input the number of columns in the matrix: ");
             Int32.TryParse(Console.ReadLine(), out n);
 
+            int[,] sumArray = new int[m, n];
+            int[] results = new int[m];
 
+            for(int i = 0; i < sumArray.GetLength(0); i++)
+            {
+                int value = 0;
+
+                for (int j = 0; j < sumArray.GetLength(1); j++)
+                {
+                    sumArray[i, j] = GetRandomNumber();
+                    value += sumArray[i, j];
+                }
+
+                results[i] = value;
+            }
+
+            Console.WriteLine("[{0}]", string.Join(", ", results));
+            Console.ReadLine();
+
+
+        }
+
+        private static int GetRandomNumber()
+        {
+            Random random = new Random();
+            int value = random.Next(-100, 100);
+            return value;
         }
     }
 }
